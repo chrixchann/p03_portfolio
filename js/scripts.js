24a65d9c0324c03033ea10cc60af7addc7692edb
Chris <ckc54@sfu.ca>
2023-03-24 01:07:13 -0700

    finished homepage

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+let currentBackground = 1;
+
+var backgroundContainer = document.querySelector(".heroContainer");
+
+const oceanButton = document.getElementById("bg1");
+const rosemaryButton = document.getElementById("bg2");
+const mountainButton = document.getElementById("bg3");
+
+oceanButton.addEventListener("click", () => {
+  //   if (bg == currentBackground) {
+  //     return;
+  //   } else {
+  //     currentBackground = 1;
+  backgroundContainer.style.backgroundImage = 'url("/img/bg1.png")';
+  //   }
+});
+
+rosemaryButton.addEventListener("click", () => {
+  //   if (bg == currentBackground) {
+  //     return;
+  //   } else {
+  //   currentBackground = 2;
+  backgroundContainer.style.backgroundImage = 'url("/img/bg2.jpg")';
+  //   }
+});
+mountainButton.addEventListener("click", () => {
+  //   if (bg == currentBackground) {
+  //     return;
+  //   } else {
+  //   currentBackground = 2;
+  backgroundContainer.style.backgroundImage = 'url("/img/bg3.jpg")';
+  //   }
+});
+
+// function background(bg) {
+//   if (bg == currentBackground) {
+//     return;
+//   }
+//   switch (bg) {
+//     case 1:
+//       currentBackground = 1;
+//       backgroundContainer.background = "url('/img/oceanbluegreen.png')";
+//     case 2:
+//       currentBackground = 2;
+//       backgroundContainer.background = 'url("/img/rosemary.jpg")';
+
+//     case 3:
+//       currentBackground = 3;
+//       backgroundContainer.background =
+//         'url("/img/milky_way_sky-1920x1080.jpg")';
+//   }
+// }
